138ce8d10e4507e5e6656d1a4c0a350235cfc598
RyoHirakawa <ryo.hirakawa@g.dreamcareer.co.jp>
2024-05-15 11:38:53 +0900

    pkgフォルダをアップ

---
+++ pkg/hello_wasm_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function say(a: number, b: number): void;
+export function generate(): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
